94e3ba59903d7eb032806c8c6cefc04a825a0f31
Gavin Rehkemper <grehkemper@esri.com>
2020-06-19 13:58:29 -0500

    example page - include map and widget

---
+++ example/components/Wrapper.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { loadModules } from 'esri-loader';
+
+import MyWidget from '../../.';
+
+export default function Wrapper({ mapView }) {
+  const [modules, setModules] = useState();
+
+  useEffect(() => {
+    // this is where we use esri-loader to pass into MyWidget as PROPS any JS API Module we need
+    loadModules([
+      'esri/widgets/Sketch',
+      'esri/geometry/geometryEngine'
+    ]).then(modules => {
+      setModules(modules);
+    });
+  }, []);
+
+  return (
+    <div className="wrapperComponent">
+      <MyWidget
+        mapView={mapView}
+        modules={modules}
+        featureLayerId={'testLayerId'}
+      ></MyWidget>
+    </div>
+  );
+}
